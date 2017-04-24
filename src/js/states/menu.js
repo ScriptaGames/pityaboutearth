29044ddff2892b45162b260718a08ca0baad2980
mwcz <mwc@clayto.com>
2017-04-24 11:27:41 -0400

    add some text to the menu

--- src/js/states/menu.js
+++ src/js/states/menu.js
@@ -11,19 +11,23 @@ class MenuState extends Phaser.State {
         const bg = this.game.add.sprite(0, 0, 'background');
         bg.tint = 0x3f3f3f;
 
-        // const logo = this.game.add.sprite(this.game.world.centerX, 300, 'logo');
-        // logo.anchor.set(0.5, 0);
+        const logo = this.game.add.sprite(this.game.world.centerX, 166, 'logo');
+        logo.anchor.set(0.5, 0);
 
-        const fontSet = `! "#$%^'()* +,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_abcdefghijklmnopqrstuvwxyz{|}~`;
-        this.font = game.add.retroFont('gelatin-font', 70, 110, fontSet, 18, 0, 0);
-        this.text = this.game.add.image(80, 80, this.font);
-        this.text.scale.set(0.5,0.5);
-        this.font.text = '';
-        this.text.tint = 0x65b219;
+        this.fontSet = `! "#$%^'()* +,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_abcdefghijklmnopqrstuvwxyz{|}~`;
 
-        this.i = 0;
-
-        this.story = `Pity about Earth...`;
+        this.story = [
+            `Our planet is a lonely speck`,
+            `in the great enveloping cosmic`,
+            `dark.`,
+            ``,
+            `In our obscurity -- in all this`,
+            `vastness, there is no hint that`,
+            `help will come from elsewhere`,
+            `to save us.`,
+            ``,
+            `                  -- Carl Sagan`,
+        ];
 
         const btnHum = game.add.button(
             game.world.centerX - 160,
@@ -38,12 +42,20 @@ class MenuState extends Phaser.State {
             this.next();
         }
 
+        this.timeBetweenLines = 400;
+        this.timePerChar = 48;
+
+        let acc = 0;
+        this.story.forEach((line, i) => {
+            this.game.time.events.add(i * this.timeBetweenLines + acc, () => {
+                this.writeLine(line, i);
+            }, this);
+            acc += line.length * this.timePerChar
+        });
+
     }
 
     update() {
-        this.i += 1;
-
-        this.font.text = this.story.slice(0, this.i);
     }
 
     next() {
@@ -52,6 +64,26 @@ class MenuState extends Phaser.State {
 
     shutdown() {
         this.music.stop();
+    }
+
+    getLine(index) {
+        return this.story[i];
+    }
+
+    writeLine(line, index) {
+        const font = game.add.retroFont('gelatin-font', 70, 110, this.fontSet, 18, 0, 0);
+        const text = this.game.add.image(80, 80, font);
+        text.scale.set(0.5,0.5);
+        font.text = '';
+        text.tint = 0x51B5E0;
+        text.position.x = 40;
+        text.position.y = index * 80 + 600;
+        let i = 0;
+
+        this.game.time.events.loop(this.timePerChar, () => {
+            font.text = line.slice(0, i);
+            i += 1;
+        }, this);
     }
 
 }
